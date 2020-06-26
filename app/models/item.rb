4e7e65f722dc5db6750f1110ba1fa0e3b6cbc675
Tresor11 <tresor.moise2001@gmail.com>
2020-06-26 13:14:51 +0200

    added the validation for the image

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@
 class Item < ApplicationRecord
   has_many :favorites, dependent: :destroy
   belongs_to :user
-  validates_presence_of :name, :description, :price, :contact
+  validates_presence_of :name, :description, :price, :contact,:image
   validates_uniqueness_of :description
   mount_uploader :image, ImageUploader
 end
