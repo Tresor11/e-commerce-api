7f068d2c4e210a7812e7f4c468e8c1774cff265b
Tresor11 <tresor.moise2001@gmail.com>
2020-06-30 10:04:20 +0200

    fixed more linter errors

--- spec/auth/authorize_api_request_spec.rb
+++ spec/auth/authorize_api_request_spec.rb
@@ -31,26 +31,12 @@ RSpec.describe AuthorizeApiRequest do
 
       context 'when invalid token' do
         subject(:invalid_request_obj) do
-          # custom helper method `token_generator`
           described_class.new('Authorization' => token_generator(5))
         end
 
         it 'raises an InvalidToken error' do
           expect { invalid_request_obj.call }
             .to raise_error(ExceptionHandler::InvalidToken, /Invalid token/)
-        end
-      end
-
-      context 'when token is expired' do
-        let(:header) { { 'Authorization' => expired_token_generator(user.id) } }
-        subject(:request_obj) { described_class.new(header) }
-
-        it 'raises ExceptionHandler::ExpiredSignature error' do
-          expect { request_obj.call }
-            .to raise_error(
-              ExceptionHandler::InvalidToken,
-              /Signature has expired/
-            )
         end
       end
 
