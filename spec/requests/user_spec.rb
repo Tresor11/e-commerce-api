6049cf21c5251f9b1817a22882e721109dd2213a
Tresor11 <tresor.moise2001@gmail.com>
2020-07-01 14:16:28 +0200

    added the tests for a valid request

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 
 RSpec.describe 'Users API', type: :request do
   let!(:user) { build(:user) }
+  let!(:image) { Rack::Test::UploadedFile.new("#{Rails.root}/spec/support/kid.jpg", 'profile') }
   let(:headers) { valid_headers.except('Authorization') }
   let(:valid_attributes) do
-    attributes_for(:user, password_confirmation: user.password)
+    attributes_for(:user, password_confirmation: user.password, image: image)
   end
 
   # User signup test suite
   describe 'POST /signup' do
-    context 'when invalid request' do
+    context 'when invalid credential' do
       before { post '/signup', params: valid_attributes.to_json, headers: headers }
-
       it 'does not create a new user' do
         expect(response).to have_http_status(422)
       end
@@ -23,6 +23,22 @@ RSpec.describe 'Users API', type: :request do
 
       it 'returns an authentication token' do
         expect(json['auth_token']).to be_nil
+      end
+    end
+
+    context 'when valid request' do
+      before { post '/signup', params: valid_attributes, headers: headers }
+
+      it 'creates a new user' do
+        expect(response).to have_http_status(201)
+      end
+
+      it 'returns success message' do
+        expect(json['message']).to match(/Account created successfully/)
+      end
+
+      it 'returns an authentication token' do
+        expect(json['auth_token']).not_to be_nil
       end
     end
 
