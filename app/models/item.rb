c7fc1ee4b6646b7d1707c47fc47552d7bb405f6a
Tresor <tresor.moise2001@gmail.com>
2021-11-02 17:23:29 +0200

    impove the liked query for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,13 @@ class Item < ApplicationRecord
   validates_presence_of :name, :description, :price, :contact, :image
   validates_uniqueness_of :description
   mount_uploader :image, ImageUploader
+
+  #query all items that have been favorited by any user
+  # def self.favorited
+  #   joins(:favorites).group('items.id')
+  # end
+
+  def self.favorited(user)
+    joins(:favorites).where(user_id: user.id).group('items.id')
+  end
 end
